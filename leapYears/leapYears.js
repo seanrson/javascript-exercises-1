adfd369ef98e2e062be3959af027113ddcc47f28
seanrson <seanrichardson@outlook.com>
2020-02-28 11:21:22 -0800

    leapYears completed

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year)
+{
+    if (year % 4 == 0) // divisible by 4?
+        if (year % 100 != 0 || year % 400 == 0) // not divisible by 100 unless divisible by 400?
+            return true;
+    return false;
 }
 
 module.exports = leapYears
