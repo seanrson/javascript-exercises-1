b6e947e1053fb2751bea75f2731cbf914dfb9f4c
seanrson <seanrichardson@outlook.com>
2020-02-28 10:57:59 -0800

    First 4 exercises completed

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, target1, target2, target3, target4)
+{
+    var arrayLen;
+    var target;
+    for (k=1; k<arguments.length; k++) // for each element argument
+    {
+        arrayLen = arr.length;
+        target = arguments[k];
+        for (i=0; i<arrayLen; i++) // go through whole array
+            if (arr[i] === target) // if we find target element
+            {
+                for (j=i; j<arrayLen; j++) // shift all remaining elements left
+                    arr[j] = arr[j+1];
+                arr = arr.slice(0, arrayLen-1) // resize array
+            }
+    }
+    return arr
 }
 
 module.exports = removeFromArray
--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(msg, num) {
+let newMsg = "";
+var i;
+for (i=0; i<num; i++)
+    newMsg += msg;
+if (num < 0)
+    newMsg = "ERROR";
+return newMsg;
 }
 
 module.exports = repeatString
